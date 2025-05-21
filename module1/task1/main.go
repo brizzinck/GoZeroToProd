20d4180ab87384a8474027dde602b0f00f0e1d06
Vlad Lavrishko <lavrishkovlad@gmail.com>
2025-05-22 01:21:44 +0300

    feat(module1/task1): implemented

--- module1/task1/main.go
+++ module1/task1/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"GoZeroToProd/module1/task1/dto"
+	"fmt"
+)
+
+type Describer interface {
+	Describe()
+}
+
+type Person interface {
+	Describer
+	GetName() string
+	GetNickname() string
+	ChangeAge(age uint8)
+}
 
 func main() {
-	fmt.Print("`GoZeroToProd/module1/task1`")
+	yaCode := dto.NewYaCode()
+	skalse := dto.NewSkalse()
+	oleksandr := dto.NewOleksandr()
+
+	fmt.Printf("Skalse`s hobby - %s\n", skalse.GetHobby())
+
+	skalse.ChangeHobby("Hard coding in `Rust` 25/8")
+
+	fmt.Printf("Skalse`s hobby changed to - %s\n", skalse.GetHobby())
+
+	oleksandr.CompleteTask()
+
+	fmt.Printf("OleKsandr completed tasks - %d\n", oleksandr.GetCompletedTasks())
+
+	oleksandr.UncompleteTask()
+
+	fmt.Printf("OleKsandr completed tasks - %d\n", oleksandr.GetCompletedTasks())
+
+	describePersons(
+		[]Person{
+			&yaCode,
+			&skalse,
+			&oleksandr,
+		})
+}
+
+func describePersons(person []Person) {
+	for i := range person {
+		fmt.Printf("Now describing %s\n", person[i].GetName())
+
+		person[i].Describe()
+	}
 }
---
+++ module1/task1/types/types.go
@@ -0,0 +1,8 @@
+package types
+
+type Sex uint8
+
+const (
+	Male Sex = iota
+	Female
+)
